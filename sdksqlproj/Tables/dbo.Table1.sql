2eff559c22dbe083154104c540363c132d6060d1
kebullen <kebullen@microsoft.com>
2022-06-20 13:35:48 -0400

    edits

--- sdksqlproj/Tables/dbo.Table1.sql
+++ sdksqlproj/Tables/dbo.Table1.sql
@@ -3,5 +3,6 @@ CREATE TABLE [dbo].[Table1]
   [Id] INT NOT NULL,
   [Moredata] VARCHAR(50) NULL,
   [YetMoredata] VARCHAR(50) NOT NULL,
+  --added after table was created, if not null, need a default (or jump through hoops)
   [YetMoreData2] VARCHAR(50) NOT NULL DEFAULT ''
 )
