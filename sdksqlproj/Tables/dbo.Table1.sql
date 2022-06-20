7c95524485882730b9db96d5ed7b52a99cd8eba7
kebullen <kebullen@microsoft.com>
2022-06-20 12:42:30 -0400

    yeah, edits

--- sdksqlproj/Tables/dbo.Table1.sql
+++ sdksqlproj/Tables/dbo.Table1.sql
@@ -2,5 +2,6 @@ CREATE TABLE [dbo].[Table1]
 (
   [Id] INT NOT NULL,
   [Moredata] VARCHAR(50) NULL,
-  [YetMoredata] VARCHAR(50) NOT NULL
+  [YetMoredata] VARCHAR(50) NOT NULL,
+  [YetMoredata2] VARCHAR(50) NOT NULL
 )
